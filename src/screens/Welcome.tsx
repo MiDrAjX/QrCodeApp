1be52a6942020ee9ab59b1f1bb55904488fa58ab
MiDrAjX <jeisonmarques78@gmail.com>
2022-07-17 16:42:43 -0300

    build: add camera and qrcode scanner, create the config to generate apk

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+'use strict';
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
 
 function Welcome() {
+  const [barcode, setBarcode] = useState('');
+  console.log(barcode);
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <QRCodeScanner
+      onRead={e => {
+        setBarcode(e);
+      }}
+      flashMode={RNCamera.Constants.FlashMode.torch}
+      topContent={
+        <Text style={styles.centerText}>
+          Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text>{' '}
+          on your computer and scan the QR code.
+        </Text>
+      }
+      bottomContent={
+        <TouchableOpacity style={styles.buttonTouchable}>
+          <Text style={styles.buttonText}>
+            {barcode ? barcode.data : 'opa'}
+          </Text>
+        </TouchableOpacity>
+      }
+    />
   );
 }
 
 export default Welcome;
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
