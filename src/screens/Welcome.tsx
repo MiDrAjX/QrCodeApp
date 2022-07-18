8c5eb97574473014381ae7e7d2e0e9b36c2d03d1
MiDrAjX <jeisonmarques78@gmail.com>
2022-07-18 16:13:53 -0300

    feat: add useConnection in the page to do the async storage requests when init the app, let only que scanner appear when the coords have value

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,56 +1,38 @@
 'use strict';
 import React from 'react';
-import {Text, TouchableOpacity, StyleSheet} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
 import {useNavigation} from '@react-navigation/core';
 import useLocation from '../hooks/useLocation';
+import useConnection from '../hooks/useConnection';
 
 function Welcome() {
   const navigation = useNavigation();
+  const Connection = useConnection();
   const {coords} = useLocation();
-  const handleBarCodeRead = (barcode: any) => {
-    navigation.navigate('Overview', {barcode, coords});
+  let scanner;
+
+  const handleBarCodeRead = async (barcode: any) => {
+    await Connection;
+    const {data} = barcode;
+    navigation.navigate('Overview', {
+      data,
+      coords,
+      callback: scanner.reactivate(),
+    });
   };
+
   return (
-    <QRCodeScanner
-      onRead={handleBarCodeRead}
-      flashMode={RNCamera.Constants.FlashMode.auto}
-      topContent={
-        <Text style={styles.centerText}>
-          Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text>{' '}
-          on your computer and scan the QR code.
-        </Text>
-      }
-      bottomContent={
-        <TouchableOpacity
-          onPress={handleBarCodeRead}
-          style={styles.buttonTouchable}>
-          <Text style={styles.buttonText}>bom dia</Text>
-        </TouchableOpacity>
-      }
-    />
+    coords.currentLatitude && (
+      <QRCodeScanner
+        onRead={handleBarCodeRead}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        ref={node => {
+          scanner = node;
+        }}
+      />
+    )
   );
 }
 
 export default Welcome;
-
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777',
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
-  },
-  buttonTouchable: {
-    padding: 16,
-  },
-});
